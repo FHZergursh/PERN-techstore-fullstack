ce0e95f13eb068ee115b4dc0b65dcd6904fc1953
FHZergursh <89704618+FHZergursh@users.noreply.github.com>
2026-03-18 15:56:08 +0000

    display products on frontend
    title, implementation isn't perfect and the images are a little wonky, but its a future improvement once actual functionality is finished.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useProductStore } from '../store/useProductStore.js'
+import { PlusCircleIcon, RefreshCwIcon } from 'lucide-react'
+import ProductCard from '../components/ProductCard.jsx'
 
 const HomePage = () => {
+  const {products, loading, error, fetchProducts} = useProductStore()
+
+  useEffect(() => {
+    fetchProducts()
+  }, [fetchProducts])
+
+  console.log("products", products)
+
+
   return (
-    <div>HomePage</div>
+    <div className='max-w-6xl mx-auto px-4 py-8'>
+      <div className='flex justify-between items-center mb-8'>
+        <button className='btn btn-primary '>
+          <PlusCircleIcon className='size-5 mr-2' />
+          Add Product
+        </button>
+        <button onClick={fetchProducts}>
+          <RefreshCwIcon className='size-5'/>
+
+        </button>
+      </div>
+      {loading ? (
+        <div className='flex justify-center items-center h-64'> 
+        <div className='loading loading-spinner loading-lg'> </div>
+        </div>
+      ) : (
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6'>
+          {products.map((product) => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+        </div>
+      )}
+    </div>
   )
 }
 
